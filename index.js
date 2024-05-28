6e958751ebd9b3271b8b2105d1a3cb3c5aa27490
pooja526526 <132282289+pooja526526@users.noreply.github.com>
2024-05-28 19:51:44 +0530

    Update index.js modified

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('You are  in AWS  Cloud. THis is Pooja..... '); //write a response to the client
+  res.write('You are  in AWS  Cloud. THis is Pooja.....
+            I have successfully completed the pipeline connectivity'); //write a response to the client
   res.end(); //end the response
 }).listen(4000); //the server object listens on port 4000
